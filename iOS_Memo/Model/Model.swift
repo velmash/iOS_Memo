2c36619833be2c0e356c4d064bba06dfc39dd57a
hyengchan <hyengchan@gmail.com>
2021-02-24 13:36:31 +0900

    Memo Class

---
+++ iOS_Memo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  iOS_Memo
+//
+//  Created by 윤형찬 on 2021/02/24.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe")
+    ]
+}
